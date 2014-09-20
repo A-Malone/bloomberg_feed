c8f7a6dae7dca34fed3bed3cc4854d5e4044d8c3
Connor Smith <connorjsmith101@gmail.com>
2014-09-20 11:25:05 -0400

    Added JSON response stuff

--- bloomberg/maindisplay/urls.py
+++ bloomberg/maindisplay/urls.py
@@ -5,5 +5,6 @@ from maindisplay import views
 
 urlpatterns = patterns ('',
 	url(r'^$',views.start, name='startPage'),
-	url(r'^(?P<company_tag>\w+)/$', views.home, name='home')
+	url(r'^(?P<company_tag>\w+)/$', views.home, name='home'),
+	url(r'^(?P<company_tag>\w+)/get/last_call=(?P<last_call>\d+)/$', views.getJSON, name="getJSON"),
 )
--- bloomberg/maindisplay/views.py
+++ bloomberg/maindisplay/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+#used for json returns
+import json
+
 
 def start(request):
 	return HttpResponse("Hello World, This is the Bloomberg app for Hack The North. Please enter a company")
@@ -7,4 +10,17 @@ def home(request,company_tag):
 	#Parse Data from twitter
 	#AJAX? How to implement that? Separate view?
 	return HttpResponse ("Hello! This page will be used to display the graphs about %s" % company_tag)
+def getJSON(request, company_tag,last_call):
+	#Data should be newer than last_call
+	#Send the company_tag to bloomberg api
+	#return json response
 
+	#temporary Test Data:
+	response_array = []
+	response_data = {}
+	response_data['value'] = 'THE VALUE OF THE THING'
+	response_data['time'] = "THE TIME OF THE THING"
+	response_array.append(response_data)
+	response_array.append(response_data)
+	response_array.append(response_data)
+	return HttpResponse(json.dumps(response_array), content_type="application/json")
