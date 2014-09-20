4630e5d908531171d5ed12c4b8dbf67ed56d65d2
A. Malone <a_s_malone@hotmail.com>
2014-09-20 19:27:31 -0400

    Graphing of financial data now working

--- bloomberg/maindisplay/static/js/graph.js
+++ bloomberg/maindisplay/static/js/graph.js
@@ -1,12 +1,11 @@
-(function(d3, $) {
-
-  alert(name);
+(function(d3, $) {  
     
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 var parseDate = d3.time.format("%d-%b-%y").parse;
+var parseFullDate = d3.time.format("%Y-%m-%dT%H:%M:%S.%L").parse;
 
 var x = d3.time.scale()
     .range([0, width]);
@@ -26,23 +25,21 @@ var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.close); });
 
-$.get("/" + name + "/last_call=0",  function(data){
-  alert(data);
-});
-
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+
+//Approach 1
+/*
 d3.tsv("/maindisplay/static/data.tsv", function(error, data) {
   data.forEach(function(d) {   
-    var spl = d['date    close'].split(" ").filter(function(n){ return n != "" });;    
+    var spl = d['date    close'].split(" ").filter(function(n){ return n != "" });    
 
     d.date = parseDate(spl[0]);
     d.close = parseInt(spl[1]);
-    console.log(d.date  + " " + d.close);
   });
 
   x.domain(d3.extent(data, function(d) { return d.date; }));
@@ -68,4 +65,37 @@ d3.tsv("/maindisplay/static/data.tsv", function(error, data) {
       .attr("class", "line")
       .attr("d", line);
 });
+*/
+//Approach 2
+$.get(data_path, function(data){  
+  data.forEach(function(d) {
+    d.date = parseFullDate(d[0]);
+    d.close = parseFloat(d[1]);
+    console.log(d.date + " " + d.close)
+  });
+
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain(d3.extent(data, function(d) { return d.close; }));
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Price ($)");
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);      
+});
+
 })(window.d3, window.jQuery);
