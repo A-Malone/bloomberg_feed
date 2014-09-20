8d1d5c078738630bd7288e5ba2389abfbe08139c
Connor Smith <connorjsmith101@gmail.com>
2014-09-20 04:55:39 -0400

    Added to the twitter example

--- twitterapitest.py
+++ twitterapitest.py
@@ -1,10 +1,18 @@
 from twython import TwythonStreamer
-
+tweetArray = []
 class MyStreamer (TwythonStreamer):
 	def on_success(self,data):
 		if 'text' in data:
 			print data['text'].encode('utf-8') #Should add the item to the database if its different enough or newer
 			#Difference processing: http://www.nltk.org/howto/wordnet.html
+			tweetArray.append(
+				{
+					'text': data['text'].encode('utf-8'),
+					'time': data['timestamp_ms'],
+					#'user': data['user_id'] How to find userID?b
+					#'sentiment': call nltk method here
+
+				})
 	def on_error(self,status_code,data):
 		print status_code
 
@@ -14,4 +22,8 @@ OAUTH_TOKEN = '1599600042-PjahYW0zM3dvp8gGWMKOhVBANEDt0XxuT5TwAwO'
 OAUTH_TOKEN_SECRET = 'leQ6kmtD46WbvIVClm16Mo3BnW8BRKWVOt0MyHVUbKJI9'
 
 stream = MyStreamer (APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-stream.statuses.filter(track='#yolo') #Streams all items that contain #yolo
+tracks = ['#yolo','swag']
+joinedTrack = ', '.join(tracks)
+print joinedTrack #Multiple tracks must be comman separated
+stream.statuses.filter(track=joinedTrack) #Streams all items that contain #yolo
+									  
