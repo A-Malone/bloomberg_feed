23d4ca908b1467c27bb85e37600a1aaced1665a1
Connor Smith <connorjsmith101@gmail.com>
2014-09-20 21:05:47 -0400

    updated time section of twitter api calls

--- twitterapitest-request.py
+++ twitterapitest-request.py
@@ -1,5 +1,5 @@
 from twython import Twython
-
+import datetime
 APP_KEY = 'LLMtc9HXCYl7Bh65f3R90QHpc'
 ACCESS_TOKEN ='AAAAAAAAAAAAAAAAAAAAAGN3aQAAAAAAhapChctFWnFZAJCbHztynwjBFvw%3DTd24FR9IdmDJMZH7QwKH2LiHXY5HP38bLSbbqrTfiD3SgV3PS0'
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
@@ -8,6 +8,8 @@ search_terms = ["MSFT", "Microsoft","Bill Gates", "Windows", "xbox", "azure", "c
 
 query  = " OR ".join(search_terms)
 print query
-tweets = twitter.search(q=query, since="2014-09-18", until="2014-09-19")#Retrieve all of thursday's tweets
+time = str(datetime.datetime.now() - datetime.timedelta(minutes=60))
+print time
+tweets = twitter.search(q="twitter", since=time)#Retrieve all of thursday's tweets
 for tweet in tweets['statuses']:
 	print tweet['text'].encode('utf-8')
